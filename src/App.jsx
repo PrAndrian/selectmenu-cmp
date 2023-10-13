0c2d5b43a5d27c31e481d3f8f62760b56e52f432
PrAndrian <princydruh@gmail.com>
2023-10-13 09:50:17 +0200

    description variant added

--- src/App.jsx
+++ src/App.jsx
@@ -16,22 +16,40 @@ const App = () => {
       <h1 className='text-3xl font-bold'>Examples</h1>
         <div className='w-1/2 flex flex-col justify-center gap-10'>
             <h2 className='text-3xl'>1) Select Menu Default</h2>
+            <div>
+              <span className='font-bold'>Default behavior with props :</span>
+              <ul>
+                <li>`id` (string, required): A unique identifier for the component.</li>
+                <li>`values` (array of objects, required): An array of options to display in the dropdown.</li>
+                <li>`inputValue` (string, required): The currently selected value.</li>
+                <li>`setter` (function, required): A function to set the selected value.</li>
+                <li>`isError` (bool, required): Indicates whether there is an error in the component.</li>
+              </ul>
+            </div>
             <SelectMenuDefault/>
             <h2 className='text-3xl'>2) Select Menu Width</h2>
+            <p>`width` (string): The width of the component (default: '100%')</p>
             <SelectMenuWidth/>
             <h2 className='text-3xl'>3) Select Menu Input Height</h2>
+            <p>`inputHeight` (string): The height of the input field (default: '45px').</p>
             <SelectMenuInputHeight/>
             <h2 className='text-3xl'>4) Select Menu Background Color</h2>
+            <p>`backgroundColor` (string): The background color of the component (default: 'transparent').</p>
             <SelectMenuBackgroundColor/>
             <h2 className='text-3xl'>5) Select Menu Text Color</h2>
+            <p>`textColor` (string): The text color of the component (default: 'black').</p>
             <SelectMenuTextColor/>
             <h2 className='text-3xl'>6) Select Menu ActiveColor</h2>
+            <p>`activeColor` (string): The background color of the selected option (default: '#D0E7B9').</p>
             <SelectMenuActiveColor/>
             <h2 className='text-3xl'>7) Select Menu Input Text Color</h2>
+            <p>`inputTextColor` (string): The text color of the input field (default: 'black').</p>
             <SelectMenuInputTextColor/>
             <h2 className='text-3xl'>8) Select Menu Input Background Color</h2>
+            <p>`inputBackgroundColor` (string): The background color of the input field (default: 'transparent').</p>
             <SelectMenuInputBackgroundColor/>
             <h2 className='text-3xl'>9) Select Menu Icon Color</h2>
+            <p>`iconColor` (string): The color of the dropdown icon (default: 'black').</p>
             <SelectMenuIconColor/>
         </div>
     </main>
